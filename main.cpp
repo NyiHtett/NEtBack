6a6cb1b8b5b7d8b2a13f352d4a77e97dce2cdc5a
Nyi Htet <nyihtet4dev@icloud.com>
2023-03-27 15:09:34 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void scramble(string str)
+{
+    
+}
+
+void swap(string s)
+{
+   string newString;
+   int i = 0;
+   while(i< s.length)
+   {
+      if(true)
+      newString += s[i];
+   }
+}
+
+int main()
+{
+    string res = "";
+    cout << "assign a line: ";
+    getline(cin, res);
+    scramble(res);
+    swap(res);
+    return 0;
+}
+
